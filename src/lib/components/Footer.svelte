def86b84ca789d7deb032d34de3b5f90adc5c4a7
David Ishmael <dishmael3@gmail.com>
2025-05-08 15:09:23 -0400

    minor content tweak

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,7 +2,7 @@
 
 <footer class="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
 	<p>
-		Built with <a
+		This site was built with <a
 			href="https://svelte.dev"
 			class="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300">Svelte 5</a
 		>
