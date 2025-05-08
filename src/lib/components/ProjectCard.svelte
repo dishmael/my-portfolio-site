09a23052ab5f19204357a398ed4ae2bc715f5d33
David Ishmael <dishmael3@gmail.com>
2025-05-07 22:23:56 -0400

    formatting

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -3,13 +3,13 @@
 </script>
 
 <div
-	class="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:group-hover/list:opacity-50 lg:hover:!opacity-100"
+	class="group relative grid gap-4 mt-8 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:group-hover/list:opacity-50 lg:hover:!opacity-100"
 >
 	<div
 		class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"
 	></div>
 	<div class="z-10 sm:order-2 sm:col-span-6">
-		<h3>{heading}</h3>
+		<h3 class="font-bold">{heading}</h3>
 		{#if children}
 			<div class="mt-2 text-sm text-slate-400">{@render children()}</div>
 		{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -201,7 +201,7 @@
 				<ProjectCard
 					heading="This Portfolio Site"
 					imgSrc="/images/portfolio-site.png"
-					skills={['Svelte 5', 'Tailwind CSS', 'Railway']}
+					skills={['Svelte 5', 'Tailwind CSS', 'Railway', 'Cloudflare']}
 				>
 					<p>
 						I typically work with React and TypeScript, but I wanted to ramp up on Svelte. So what
